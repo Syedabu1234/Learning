cdfbea4202593c4cd6c3e29f4be51df5f03a95b2
abu <syed8395@gmail.com>
2023-03-24 11:55:23 +0530

    jar File

---
+++ my-war/src/main/java/com/mywar/app/App.java
@@ -0,0 +1,13 @@
+package com.mywar.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
